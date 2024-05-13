dea54a4f13914426d5b7d448f06118dfc5aa7c5e
bill-BKH <156478782+bill-BKH@users.noreply.github.com>
2024-05-13 17:39:41 +0330

    Update views.py
    testing merge conflicts

--- music/views.py
+++ music/views.py
@@ -6,7 +6,7 @@ def home(request):
     all_music = Music.objects.all()
     return render(request, 'music/index.html', {'musics':all_music})
 def rap(request):
-    return HttpResponse('rap ...')
+    return HttpResponse('rat ...')
 
 def gamgin(request):
     all_music = Music.objects.filter(genres='gamgin')
